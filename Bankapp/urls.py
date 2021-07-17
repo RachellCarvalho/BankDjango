c47099796d2fa464df08449cd52383943a6dfe99
Rachel C <rachelpetercarvalho070919@gmail.com>
2021-07-17 17:29:23 +0530

    initial commit

---
+++ Bankapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from Bankapp import views
+
+urlpatterns = [
+    path("",views.index,name="home"),
+    path("view_customer", views.view_customer, name="view_customer"),
+    path("make_transaction", views.make_transaction, name="make_transaction"),
+    path("history", views.history, name="history")
+
+]
